289049690030c03aef915ced404f224401fad0a2
OrkenWhite <orkenwhite@disroot.org>
2023-02-09 18:09:39 +0200

    Removed unneeded import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:polling_booth/screens/home_screen.dart';
 
 void main() {
